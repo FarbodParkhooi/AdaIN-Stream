aed72dbb32c979aedfc25f888ff9b2b3b002bcf7
Farbod Parkhooi <farbodparkhooi@duck.com>
2026-07-25 06:35:35 +0330

    Added configs.py

---
+++ utilities/configs.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+
+# Creating frozen class for constant configs
+@dataclass(frozen=True)
+class Configs():
+    """
+    All configuration needed for Generator, Discriminator, Optimizer, Dataset, and Training process
+
+    * Dangerous configurations have a '#*' at the end
+    """
+
+    ## Dataset
+    content_images_directory = "../content_images"
+    style_images_directory = "../style_images"
+    content_batch_size = 4
+    content_numworkers = 8
